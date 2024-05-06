3f620f757532130ca213a2956b0bddaf1f437f4c
Van Huy <vanhuytran241103@gmail.com>
2024-05-06 19:15:44 +0700

    init project

---
+++ Read Holding Registers/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboPort = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboBaud = new System.Windows.Forms.ComboBox();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.btnOFF = new System.Windows.Forms.Button();
+            this.txtCRCReceive = new System.Windows.Forms.TextBox();
+            this.txtCRCSend = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCaculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port:";
+            // 
+            // cboPort
+            // 
+            this.cboPort.FormattingEnabled = true;
+            this.cboPort.Location = new System.Drawing.Point(111, 30);
+            this.cboPort.Name = "cboPort";
+            this.cboPort.Size = new System.Drawing.Size(121, 24);
+            this.cboPort.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(626, 19);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(102, 24);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(626, 50);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(102, 24);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(111, 86);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(483, 24);
+            this.txtMessage.TabIndex = 3;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(368, 268);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(200, 24);
+            this.btnReceive.TabIndex = 2;
+            this.btnReceive.Text = "Receive";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Location = new System.Drawing.Point(111, 130);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.Size = new System.Drawing.Size(483, 22);
+            this.txtReceive.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Send:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Receive:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Baud rate:";
+            // 
+            // cboBaud
+            // 
+            this.cboBaud.FormattingEnabled = true;
+            this.cboBaud.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.cboBaud.Location = new System.Drawing.Point(364, 27);
+            this.cboBaud.Name = "cboBaud";
+            this.cboBaud.Size = new System.Drawing.Size(121, 24);
+            this.cboBaud.TabIndex = 1;
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(643, 169);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(75, 23);
+            this.btnOn.TabIndex = 4;
+            this.btnOn.Text = "ON";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // btnOFF
+            // 
+            this.btnOFF.Location = new System.Drawing.Point(643, 214);
+            this.btnOFF.Name = "btnOFF";
+            this.btnOFF.Size = new System.Drawing.Size(75, 23);
+            this.btnOFF.TabIndex = 4;
+            this.btnOFF.Text = "OFF";
+            this.btnOFF.UseVisualStyleBackColor = true;
+            this.btnOFF.Click += new System.EventHandler(this.btnOFF_Click);
+            // 
+            // txtCRCReceive
+            // 
+            this.txtCRCReceive.Location = new System.Drawing.Point(111, 221);
+            this.txtCRCReceive.Multiline = true;
+            this.txtCRCReceive.Name = "txtCRCReceive";
+            this.txtCRCReceive.Size = new System.Drawing.Size(483, 22);
+            this.txtCRCReceive.TabIndex = 7;
+            this.txtCRCReceive.TextChanged += new System.EventHandler(this.txtCRCReceive_TextChanged);
+            // 
+            // txtCRCSend
+            // 
+            this.txtCRCSend.Location = new System.Drawing.Point(111, 177);
+            this.txtCRCSend.Multiline = true;
+            this.txtCRCSend.Name = "txtCRCSend";
+            this.txtCRCSend.Size = new System.Drawing.Size(483, 24);
+            this.txtCRCSend.TabIndex = 8;
+            this.txtCRCSend.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "CRC Receive:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "CRC Send:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // btnCaculate
+            // 
+            this.btnCaculate.Location = new System.Drawing.Point(128, 268);
+            this.btnCaculate.Name = "btnCaculate";
+            this.btnCaculate.Size = new System.Drawing.Size(191, 24);
+            this.btnCaculate.TabIndex = 9;
+            this.btnCaculate.Text = "Caculate CRC & Send";
+            this.btnCaculate.UseVisualStyleBackColor = true;
+            this.btnCaculate.Click += new System.EventHandler(this.btnCaculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 315);
+            this.Controls.Add(this.btnCaculate);
+            this.Controls.Add(this.txtCRCReceive);
+            this.Controls.Add(this.txtCRCSend);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnOFF);
+            this.Controls.Add(this.btnOn);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cboBaud);
+            this.Controls.Add(this.cboPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboBaud;
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Button btnOFF;
+        private System.Windows.Forms.TextBox txtCRCReceive;
+        private System.Windows.Forms.TextBox txtCRCSend;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCaculate;
+    }
+}
+
---
+++ Read Holding Registers/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        string nhan = String.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cboPort.Items.AddRange(ports);
+            cboPort.SelectedIndex = 0;
+            btnClose.Enabled = false;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = false ;
+            btnClose.Enabled = true ;
+            try
+            {
+                serialPort1.PortName = cboPort.Text;
+                serialPort1.BaudRate = Convert.ToInt32(cboBaud.Text);
+                serialPort1.Open();
+                txtMessage.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                   
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private byte[] StringToByteArray(string hex)
+        {
+            hex = hex.Replace(" ", ""); // Loại bỏ khoảng trắng nếu có
+            int numberChars = hex.Length;
+            byte[] bytes = new byte[numberChars / 2];
+            for (int i = 0; i < numberChars; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+            return bytes;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    byte[] receivedBytes = ReadFromSerialPort();
+
+                    // Hiển thị chuỗi hex trong TextBox
+                    txtReceive.Text = this.Display(receivedBytes);
+                    txtCRCReceive.Text = string.Format("{0:X2} {1:X2}", receivedBytes[receivedBytes.Length - 2], receivedBytes[receivedBytes.Length - 1]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private byte[] ReadFromSerialPort()
+        {
+            // Khởi tạo một buffer để lưu dữ liệu đọc được
+            byte[] buffer = new byte[serialPort1.BytesToRead];
+
+            // Đọc dữ liệu từ cổng serial và lưu vào buffer
+            serialPort1.Read(buffer, 0, buffer.Length);
+
+            return buffer;
+        }
+
+        // Phương thức chuyển đổi mảng byte thành chuỗi hex
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen)
+            {
+                serialPort1.Write("A");
+            }
+        }
+
+        private void btnOFF_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write("D");
+            }
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            nhan = serialPort1.ReadExisting();
+            // ham ngat trong c#
+            this.Invoke(new EventHandler(DoUpdate));
+        }
+        private void DoUpdate(object sender, EventArgs e)
+        {
+            if (nhan.Length > 0)
+            {
+                Invoke(new MethodInvoker(() => txtReceive.Text = nhan));
+            }
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCaculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    // Lấy dữ liệu hex từ TextBox
+                    string hexData = txtMessage.Text;
+
+                    // Chuyển đổi chuỗi hex thành mảng byte
+                    byte[] byteArray = StringToByteArray(hexData);
+
+                    byte[] frame = this.ReadHoldingRegister(byteArray);
+
+                    txtCRCSend.Text = string.Format("{0:X2} {1:X2}", frame[frame.Length - 2], frame[frame.Length - 1]);
+                    txtMessage.Text = this.Display(frame);
+
+                    // Di chuyển con trỏ về cuối TextBox
+                    txtMessage.SelectionStart = txtMessage.TextLength;
+
+                    // Tập trung vào TextBox
+                    txtMessage.Focus();
+
+                    serialPort1.Write(frame, 0, frame.Length);
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCRCReceive_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private byte[] ReadHoldingRegister(byte[] byteArray)
+        {
+            byte[] frame = new byte[8];
+            frame[0] = byteArray[0];
+            frame[1] = byteArray[1];
+            frame[2] = byteArray[2];
+            frame[3] = byteArray[3];
+            frame[4] = byteArray[4];
+            frame[5] = byteArray[5];
+            byte[] crc = this.CaculateCRC(frame);
+            frame[6] = crc[0];
+            frame[7] = crc[1];
+            return frame;
+        }
+
+        private byte[] CaculateCRC(byte[] frame)
+        {
+            byte[] result = new byte[2];
+            ushort CRCFull = 0xFFFF;
+            char CRCLSB;
+            for (int i = 0; i < frame.Length - 2; i++)
+            {
+                CRCFull = (ushort)(CRCFull ^ frame[i]);
+
+                for (int j = 0; j < 8; j++)
+                {
+                    CRCLSB = (char)(CRCFull & 0x0001);
+                    CRCFull = (ushort)((CRCFull >> 1) & 0x7FFF);
+
+                    if (CRCLSB == 1)
+                    {
+                        CRCFull = (ushort)(CRCFull ^ 0xA001);
+                    }
+                }
+            }
+            result[1] = (byte)((CRCFull >> 8) & 0xFF);
+            result[0] = (byte)(CRCFull & 0xFF);
+            return result;
+        }
+
+        private string Display(byte[] frame)
+        {
+            string result = string.Empty;
+            foreach (byte item in frame)
+            {
+                result += string.Format("{0:X2} ", item);
+            }
+            return result;
+        }
+    }
+}
